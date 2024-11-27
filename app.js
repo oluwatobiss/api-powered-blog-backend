059817f9666eb1eb3a0dd2cfa885c60338bc43a8
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2024-11-27 18:38:50 +0100

    chore: Get port number from environment variable

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT;
 
 app.get("/", (req, res) => res.send("Helloo!!!"));
 
-app.listen("3000", () => console.log("Server listening at port 3000"));
+app.listen(port, () =>
+  console.log(`Server listening for requests at port: ${port}!`)
+);
