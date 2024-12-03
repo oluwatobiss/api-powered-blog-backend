2f96996d74e7e727f3d088ab3f8d848370ad4d48
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2024-12-03 14:50:02 +0100

    feat: Create prisma schema

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+const dotenvExpand = require("dotenv-expand");
 const express = require("express");
+
+dotenvExpand.expand(dotenv.config());
 
 const app = express();
 const port = process.env.PORT;
