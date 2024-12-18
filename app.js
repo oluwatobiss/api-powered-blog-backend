61eb00de60ae456f7b4b727351fcdfdab63788e4
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2024-12-18 18:31:51 +0100

    build: Uninstall express-session

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const dotenv = require("dotenv");
 const dotenvExpand = require("dotenv-expand");
 const cors = require("cors");
 const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
 const userRouter = require("./routes/user");
 const postRouter = require("./routes/post");
 const commentRouter = require("./routes/comment");
@@ -16,14 +14,6 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-app.use(passport.session());
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
