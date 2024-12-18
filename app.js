1c42ff94930626fa3843de746c385f8267bcb179
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2024-12-18 15:55:38 +0100

    feat: Confirm if the incoming request has an authenticated session cookie data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dotenvExpand = require("dotenv-expand");
 const cors = require("cors");
 const express = require("express");
 const session = require("express-session");
+const passport = require("passport");
 const userRouter = require("./routes/user");
 const postRouter = require("./routes/post");
 const commentRouter = require("./routes/comment");
@@ -16,13 +17,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  session({ 
+  session({
     secret: process.env.SESSION_SECRET,
-    resave: false, 
-    saveUninitialized: false, 
+    resave: false,
+    saveUninitialized: false,
   })
 );
-
+app.use(passport.session());
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
