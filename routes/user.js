61683d4392852a7eeec5ee2f5295be946e00fb80
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-29 16:18:57 +0100

    feat: Prevent unauthenticated users from accessing the /users route's data

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const controller = require("../controllers/user");
+const middleware = require("../middlewares/authentication");
 
 const router = Router();
 
-router.get("/", controller.getUsers);
-router.get("/:id", controller.getUser);
-router.post("/", controller.createUser);
-router.put("/:id", controller.updateUser);
-router.delete("/:id", controller.deleteUser);
+router.get("/", middleware.authenticateUser, controller.getUsers);
+router.get("/:id", middleware.authenticateUser, controller.getUser);
+router.post("/", middleware.authenticateUser, controller.createUser);
+router.put("/:id", middleware.authenticateUser, controller.updateUser);
+router.delete("/:id", middleware.authenticateUser, controller.deleteUser);
 
 module.exports = router;
