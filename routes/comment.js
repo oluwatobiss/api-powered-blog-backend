ab4997f7448c975ab3058b0a6f4708c6721d5246
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-29 16:13:15 +0100

    feat: Prevent unauthenticated users from accessing the /comments route's data

--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,20 @@
 const { Router } = require("express");
 const controller = require("../controllers/comment");
+const middleware = require("../middlewares/authentication");
 
 const router = Router({ mergeParams: true });
 
-router.get("/", controller.getComments);
-router.post("/", controller.createComment);
-router.put("/:commentId", controller.updateComment);
-router.delete("/:commentId", controller.deleteComment);
+router.get("/", middleware.authenticateUser, controller.getComments);
+router.post("/", middleware.authenticateUser, controller.createComment);
+router.put(
+  "/:commentId",
+  middleware.authenticateUser,
+  controller.updateComment
+);
+router.delete(
+  "/:commentId",
+  middleware.authenticateUser,
+  controller.deleteComment
+);
 
 module.exports = router;
