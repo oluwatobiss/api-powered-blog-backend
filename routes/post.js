c7586e609bbd551a3154ca987bbe329ed9b17eb4
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-29 16:05:46 +0100

    feat: Prevent unauthenticated users from accessing the /posts route's data

--- routes/post.js
+++ routes/post.js
@@ -4,15 +4,19 @@ const middleware = require("../middlewares/authentication");
 
 const router = Router();
 
-router.get("/", controller.getPosts);
-router.get("/authors/:authorId", controller.getAuthorPosts);
-router.get("/:id", controller.getPost);
+router.get("/", middleware.authenticateUser, controller.getPosts);
+router.get(
+  "/authors/:authorId",
+  middleware.authenticateUser,
+  controller.getAuthorPosts
+);
+router.get("/:id", middleware.authenticateUser, controller.getPost);
 router.post(
   "/authors/:authorId",
   middleware.authenticateUser,
   controller.createPost
 );
-router.put("/:id", controller.updatePost);
-router.delete("/:id", controller.deletePost);
+router.put("/:id", middleware.authenticateUser, controller.updatePost);
+router.delete("/:id", middleware.authenticateUser, controller.deletePost);
 
 module.exports = router;
